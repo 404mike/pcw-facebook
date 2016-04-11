4d693f6dbb607ba829914e141bfdc5d614d6407f
Michael Jones <michaeljones401@gmail.com>
2016-04-11 17:15:49 +0100

    first commit

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+namespace llgc;
+
+require __DIR__ . '/vendor/autoload.php';
+
+use llgc\src;
+
+class Main {
+
+  public function __construct()
+  {
+    echo 234;
+  }
+
+}
+
+
+(new Main());
